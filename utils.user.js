fe3c23b26d34afcf6cb81e9e7224e9df7dd7c431
ElijahPepe <business@elijahpepe.com>
2022-04-02 20:55:46 -0700

    Refactor and use fetch to get vandals

--- utils.user.js
+++ utils.user.js
@@ -7,41 +7,63 @@
 // @match        https://hot-potato.reddit.com/embed*
 // ==/UserScript==
 if (window.top !== window.self) {
-    window.addEventListener('load', () => {
-      document.onkeydown = keydown; 
-      
-      document.getElementsByTagName("mona-lisa-embed")[0]
+  window.addEventListener(
+    'load',
+    () => {
+      document.onkeydown = keydown
 
-      function keydown (evt) {
-        if (!evt) evt = event;
+      document.getElementsByTagName('mona-lisa-embed')[0]
+
+      function keydown(evt) {
+        if (!evt) evt = event
 
         if (evt.ctrlKey && evt.shiftKey && evt.keyCode === 70) {
-          const coordinates = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-coordinates")[0].shadowRoot.children[0]
-          navigator.clipboard.writeText(coordinates.textContent.split(" ")[1].replace(/[{()}]/g, ''));
+          const coordinates = document
+            .getElementsByTagName('mona-lisa-embed')[0]
+            .shadowRoot.children[0].getElementsByTagName('mona-lisa-coordinates')[0].shadowRoot
+            .children[0]
+          navigator.clipboard.writeText(
+            coordinates.textContent.split(' ')[1].replace(/[{()}]/g, '')
+          )
         }
       }
-    }, false);
-    window.addEventListener('load', () => {
-      // TODO: Not this
-      const vandals = ["Crazy_Ate_", "Mineymma", "-Mopp", "d-faulty", "Benavoe", "Viewer23", "TheBentoVixen", "HermitCrabParty", "ColoradaStateSupreme", "Loose_Salamanders", "IronyIsFunny234", "CheshireCorner", "Melyndrome", "Available_Humor_73", "ZryptoYT", "veryshortmidget", "DiscordMLP2", "Neeebel", "Bugborger", "Middle_Drink9562", "Infamous_Sun_9854", "DisasterNew9425", "EggCodex", "ICantWinSorry", "BungleFunker", "givememorebeans", "organicgobbler", "livesincondemnation", "PluralPanFry", "boardinglyf", "eybers_"];
+    },
+    false
+  )
+  window.addEventListener(
+    'load',
+    () => {
+      let array
+      fetch('https://raw.githubusercontent.com/ElijahPepe/rplace-vandals/main/vandals.txt')
+        .then(function (response) {
+          return response.text()
+        })
+        .then(function (html) {
+          array = html.split(/\n|\r/g)
+        })
 
-      const button = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].getElementsByClassName("profile-button")[0]
+      const button = document
+        .getElementsByTagName('mona-lisa-embed')[0]
+        .shadowRoot.children[0].getElementsByTagName('mona-lisa-camera')[0]
+        .shadowRoot.children[0].getElementsByClassName('profile-button')[0]
 
-      const label = document.createElement("div");
-      label.innerHTML = "(vandal!)";
-      
-      var observer = new MutationObserver(function(mutations) {
+      const label = document.createElement('div')
+      label.innerHTML = '(vandal!)'
+
+      var observer = new MutationObserver(function (mutations) {
         if (vandals.includes(button.innerText.slice(2))) {
-          observer.disconnect();
-          button.appendChild(label);
-          observer.observe(button, config);
+          observer.disconnect()
+          button.appendChild(label)
+          observer.observe(button, config)
         } else if (button.contains(label)) {
-          button.removeChild(label); 
+          button.removeChild(label)
         }
-      });
-      
-      var config = { childList: true };
-      
-      observer.observe(button, config);
-    }, false);
+      })
+
+      var config = { childList: true }
+
+      observer.observe(button, config)
+    },
+    false
+  )
 }
